da778061a7643db78c5440e39f9089c8402158f7
Jeremy Huff <jeremyhuff@samanthas-macbook-pro.local>
2015-09-17 16:48:52 -0700

    Added bridging header to expose AFNetworking.

---
+++ GABootcamp/GABootcamp/GABootcamp-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  GABootcamp-Bridging-Header.h
+//  GABootcamp
+//
+//  Created by Jeremy Huff on 9/17/15.
+//  Copyright © 2015 Jeremy Huff. All rights reserved.
+//
+
+#import "AFNetworking.h"
