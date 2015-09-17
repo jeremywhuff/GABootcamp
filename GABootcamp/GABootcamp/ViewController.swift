eeaae37637ddf73aed2d4de0e4e0a8283b848dd0
Jeremy Huff <jeremyhuff@samanthas-macbook-pro.local>
2015-09-17 13:04:34 -0700

    Button now changes view color.

--- GABootcamp/GABootcamp/ViewController.swift
+++ GABootcamp/GABootcamp/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        let redView = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-//        redView.backgroundColor = UIColor.redColor()
-//        self.view.addSubview(redView)
+    @IBOutlet weak var coloredView: UIView!
+    
+    @IBAction func changeColorButtonPushed(sender: AnyObject) {
+        if coloredView.backgroundColor == UIColor.redColor() {
+            coloredView.backgroundColor = UIColor.greenColor()
+        }
+        else {
+            coloredView.backgroundColor = UIColor.redColor()
+        }
     }
 }
 
