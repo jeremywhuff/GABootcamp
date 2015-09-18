bf9b2ed69a664fe9a78ace26fe178b8d0045eeb6
Jeremy Huff <jeremyhuff@samanthas-macbook-pro.local>
2015-09-17 17:33:01 -0700

    Venue names are now printed.

--- GABootcamp/GABootcamp/GAModel.swift
+++ GABootcamp/GABootcamp/GAModel.swift
@@ -18,6 +18,17 @@ class GAModel {
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation, response: AnyObject) -> Void in
                 print("Success")
+                
+                // Print the returned names
+                let responseDictionary = response as! [NSObject:AnyObject]
+                
+                let level1 = responseDictionary["response"] as! [NSObject:AnyObject]
+                let venues = level1["venues"] as! [AnyObject]
+                for venue in venues {
+                    let venueDict = venue as! [NSObject:AnyObject]
+                    let name = venueDict["name"] as! String
+                    print(name)
+                }
             }) { (operation: AFHTTPRequestOperation, error: NSError) -> Void in
                 print("Failure")
         }
